7f68a587fc5539d2634ea5318e0efee622a3f442
Muhammad Awais <awaisshaikh94@gmail.com>
2020-11-07 10:08:09 +0500

    Updates

---
+++ precache-manifest.222490ebb6e7a6c981073be56102ac69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052b2d56853807bf2895763a130420be",
+    "url": "/react-firebase/index.html"
+  },
+  {
+    "revision": "3e52c39c9525c6743ed6",
+    "url": "/react-firebase/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "1a5845f4ad4b2e741a87",
+    "url": "/react-firebase/static/css/main.88fd16dd.chunk.css"
+  },
+  {
+    "revision": "3e52c39c9525c6743ed6",
+    "url": "/react-firebase/static/js/2.a9be32a0.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/react-firebase/static/js/2.a9be32a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a5845f4ad4b2e741a87",
+    "url": "/react-firebase/static/js/main.a93f4a55.chunk.js"
+  },
+  {
+    "revision": "69f34f0fc594c12b4bae",
+    "url": "/react-firebase/static/js/runtime-main.de05ab3e.js"
+  }
+]);
